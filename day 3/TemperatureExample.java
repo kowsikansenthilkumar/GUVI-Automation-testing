38322b8fb0a54c8b37c94d26175cafcf420786d4
Kowsikan <kowsiks2005@gmail.com>
2026-04-09 09:56:05 +0530

    refactor: remove unnecessary comments in TemperatureExample class

--- day 3/TemperatureExample.java
+++ day 3/TemperatureExample.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class TemperatureExample {
     public static void main(String[] args) {
 
-        // List to store temperatures (duplicates allowed)
         List<Integer> tempList = new ArrayList<>();
         tempList.add(30);
         tempList.add(25);
@@ -11,13 +10,8 @@ public class TemperatureExample {
         tempList.add(28);
         tempList.add(25);
 
-        // Set to remove duplicate temperatures
         Set<Integer> tempSet = new HashSet<>(tempList);
-
-        // Display original list
         System.out.println("Temperature List: " + tempList);
-
-        // Display unique temperatures
         System.out.println("Unique Temperatures: " + tempSet);
     }
 }
